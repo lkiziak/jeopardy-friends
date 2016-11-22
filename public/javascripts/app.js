53257e953c056f9740f960594249fa64cf397039
blair dowis <blairdow@gmail.com>
2016-11-22 10:24:00 -0800

    fixed conflicts in app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var socket = io();
   socket.on('add-message', function (data) {
     addMessage(data);
+      
   });
 
   document.getElementById('btn-send-msg').addEventListener('click', function() {
@@ -17,8 +18,30 @@ document.addEventListener("DOMContentLoaded", function() {
     newMsg.value = '';
   });
 
-  function addMessage(data) {
+
+function addMessage(data) {
+
     messages.innerHTML += ['<li class="chat-message"><span class="userEmail">', data.name, '</span>: <span class="msg-content">', data.msg, '</span></li>'].join('');    
   }
 
 });
+
+function makeQuestion(obj) {
+return `<h1>${obj.category}</h1>
+      <p>${obj.question}</p>
+      <hr>
+      <small>${obj.answer}</small>
+      `;
+}
+
+
+function generateQuestion(){
+  $.get('/api/random').then(function(data) {
+  $('#question').html("")
+  $('#question').prepend(makeQuestion(data.info));
+  }, function(err) {console.error(err);})
+}
+//setInterval(generateQuestion, 9000);
+generateQuestion()
+
+
