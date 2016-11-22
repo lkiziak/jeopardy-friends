2c04edd1f67e2cb6d330ef2916981bd772a52809
James Costello <jcostello10@gmail.com>
2016-11-22 14:19:18 -0800

    some more functionaliy in app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,18 +3,48 @@ document.addEventListener("DOMContentLoaded", function() {
   var newMsg = document.getElementById('new-msg');
   var userName = document.getElementById('user-name');
   var userEmail = document.getElementById('user-email').innerHTML;
+  var answer;
 
   var socket = io();
   socket.on('add-message', function (data) {
     addMessage(data);
-
   });
+
+function generateQuestion(){
+  $.get('/api/random').then(function(data) {
+  $('#question').html("")
+  $('#category').html("")
+  $('#question').prepend(makeQuestion(data.info));
+  $('#category').prepend(makeCategory(data.info));
+  answer = data.info.answer
+  console.log(answer)
+  }, function(err) {console.error(err);})
+}
+
+setInterval(generateQuestion, 15000);
+
+
+function checkAnswer(){
+    console.log('answer: ', answer)
+    var msg = $('.msg-content')
+    var name = $('.username')
+
+  for (var i = 0; i < msg.length; i++){
+    if (answer == msg[i].innerHTML) {
+       name = msg[i].previousElementSibling.innerHTML
+      console.log(name + ' WAS CORRECT!')
+    } else {
+      console.log('KEEP GUESSING')
+    }
+  }
+}
 
   document.getElementById('btn-send-msg').addEventListener('click', function() {
     socket.emit('add-message', {
       name: userEmail,
       msg: newMsg.value
     });
+      checkAnswer();
     newMsg.value = '';
   });
 
@@ -30,58 +60,42 @@ document.addEventListener("DOMContentLoaded", function() {
 function makeQuestion(obj) {
    return `
           <p>${obj.question}</p>
-<<<<<<< HEAD
-          <hr>
-          <small id='answer'>${obj.answer}</small>
-=======
->>>>>>> master
+
           `;
 }
 
 function makeCategory(obj) {
     return `<p>${obj.category}</p>`
 }
-var answer
 
-function generateQuestion(){
-  $.get('/api/random').then(function(data) {
-  $('#question').html("")
-  $('#category').html("")
-  $('#question').prepend(makeQuestion(data.info));
-<<<<<<< HEAD
-  answer = data.info.answer
-  console.log(answer)
-  }, function(err) {console.error(err);})
-}
+// function generateQuestion(){
+//   $.get('/api/random').then(function(data) {
+//   $('#question').html("")
+//   $('#category').html("")
+//   $('#question').prepend(makeQuestion(data.info));
+//   $('#category').prepend(makeCategory(data.info));
+//   answer = data.info.answer
+//   console.log(answer)
+//   }, function(err) {console.error(err);})
+// }
 
-setInterval(generateQuestion, 9000);
+// setInterval(generateQuestion, 9000);
 
-//When submit button is pushed trigger checkAnswer()
 
-function checkAnswer(){
-    console.log(answer)
-    var msg = $('.msg-content')
-    var name = $('.username')
-=======
-  $('#category').prepend(makeCategory(data.info));
-  }, function(err) {console.error(err);})
-}
+// function checkAnswer(){
+//     // console.log(answer)
+//     var msg = $('.msg-content')
+//     var name = $('.username')
 
-//setInterval(generateQuestion, 9000);
-generateQuestion()
->>>>>>> master
-
-  for (var i = 0; i < msg.length; i++){
-    // Specify which user's msg matches the answer first
-
-    if (answer == msg[i].innerHTML) {
-       name = msg[i].previousElementSibling.innerHTML
-      console.log(name + ' WAS CORRECT!')
-    } else {
-      console.log('KEEP GUESSING')
-    }
-  }
-}
+//   for (var i = 0; i < msg.length; i++){
+//     if (answer == msg[i].innerHTML) {
+//        name = msg[i].previousElementSibling.innerHTML
+//       console.log(name + ' WAS CORRECT!')
+//     } else {
+//       console.log('KEEP GUESSING')
+//     }
+//   }
+// }
 
 
 
